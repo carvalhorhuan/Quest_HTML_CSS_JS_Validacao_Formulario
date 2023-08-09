49c18d9f3f587fb4df65ca54b5977fc053d692c3
Rhuan Carvalho <carvalho.rhuan@gmail.com>
2023-08-09 13:03:07 -0300

    js operando

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,47 @@
-const form = document.getElementById('form');
-const nome = document.getElementById('nome');
-const email = document.getElementById('email');
-const telefone = document.getElementById('phone');
-const observacao = document.getElementById('observacoes');
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+    const nameInput = document.getElementById('nome');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone');
+    const observacoesInput = document.getElementById('observacoes');
+    const submitButton = document.getElementById('submit');
 
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
 
-form.addEventListener('submit')
+        if (nameInput.value.trim() === '') {
+            nameInput.parentElement.classList.add('error');
+        } else {
+            nameInput.parentElement.classList.remove('error');
+            nameInput.parentElement.classList.add('correct');
+        }
+
+        if (emailInput.value.trim() === '') {
+            emailInput.parentElement.classList.add('error');
+        } else {
+            emailInput.parentElement.classList.remove('error');
+            emailInput.parentElement.classList.add('correct');
+        }
+
+        if (phoneInput.value.trim() === '') {
+            phoneInput.parentElement.classList.add('error');
+        } else {
+            phoneInput.parentElement.classList.remove('error');
+            phoneInput.parentElement.classList.add('correct');
+        }
+
+        if (observacoesInput.value.trim() === '') {
+            observacoesInput.parentElement.classList.add('error');
+        } else {
+            observacoesInput.parentElement.classList.remove('error');
+            observacoesInput.parentElement.classList.add('correct');
+        }
+
+        const inputGroups = form.querySelectorAll('.input-group');
+        const hasError = Array.from(inputGroups).some(group => group.classList.contains('error'));
+
+        if (!hasError) {
+            alert('Mensagem enviada com sucesso!');
+        }
+    });
+});
