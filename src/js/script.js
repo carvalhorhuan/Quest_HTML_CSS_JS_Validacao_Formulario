1b03a54598f1e3dec4c60401034ed68e746f577d
Rhuan Carvalho <carvalho.rhuan@gmail.com>
2023-08-09 14:07:35 -0300

    responsividade e visualização do erro abaixo dos inputs

--- src/js/script.js
+++ src/js/script.js
@@ -11,37 +11,45 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if (nameInput.value.trim() === '') {
             nameInput.parentElement.classList.add('error');
+            nameInput.nextElementSibling.style.visibility = 'visible';
         } else {
             nameInput.parentElement.classList.remove('error');
             nameInput.parentElement.classList.add('correct');
+            nameInput.nextElementSibling.style.visibility = 'hidden';
         }
 
         if (emailInput.value.trim() === '') {
             emailInput.parentElement.classList.add('error');
+            emailInput.nextElementSibling.style.visibility = 'visible';
         } else {
             emailInput.parentElement.classList.remove('error');
             emailInput.parentElement.classList.add('correct');
+            emailInput.nextElementSibling.style.visibility = 'hidden';
         }
 
         if (phoneInput.value.trim() === '') {
             phoneInput.parentElement.classList.add('error');
+            phoneInput.nextElementSibling.style.visibility = 'visible';
         } else {
             phoneInput.parentElement.classList.remove('error');
             phoneInput.parentElement.classList.add('correct');
+            phoneInput.nextElementSibling.style.visibility = 'hidden';
         }
 
         if (observacoesInput.value.trim() === '') {
             observacoesInput.parentElement.classList.add('error');
+            observacoesInput.nextElementSibling.style.visibility = 'visible';
         } else {
             observacoesInput.parentElement.classList.remove('error');
             observacoesInput.parentElement.classList.add('correct');
+            observacoesInput.nextElementSibling.style.visibility = 'hidden';
         }
 
-        const inputGroups = form.querySelectorAll('.input-group');
-        const hasError = Array.from(inputGroups).some(group => group.classList.contains('error'));
+        //const inputGroups = form.querySelectorAll('.input-group');
+        //const hasError = Array.from(inputGroups).some(group => group.classList.contains('error'));
 
-        if (!hasError) {
-            alert('Mensagem enviada com sucesso!');
-        }
+        //if (!hasError) {
+        //    alert('Mensagem enviada com sucesso!');
+        //}
     });
 });
