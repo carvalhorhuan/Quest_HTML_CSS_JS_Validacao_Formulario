f0ec0a8472009fd2db95bb3721317b760a690b54
Rhuan Carvalho <carvalho.rhuan@gmail.com>
2023-08-09 11:19:18 -0300

    primeiro commit

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('form');
+const nome = document.getElementById('nome');
+const email = document.getElementById('email');
+const telefone = document.getElementById('phone');
+const observacao = document.getElementById('observacoes');
+
+
+form.addEventListener('submit')
